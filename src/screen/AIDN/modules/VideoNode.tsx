cf8dc7253453757f573848869825aa94f177b040
kvinitkarmkar <vinitkumarkarmkar836@gmail.com>
2026-06-15 15:55:14 +0530

    Add VideoNode component for video playback

--- src/screen/AIDN/modules/VideoNode.tsx
+++ src/screen/AIDN/modules/VideoNode.tsx
@@ -1,1 +1,46 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
 
+import Video from 'react-native-video';
+
+interface VideoNodeProps {
+  url: string;
+}
+
+const { width, height } = Dimensions.get('window');
+
+const VideoNode = ({
+  url,
+}: VideoNodeProps) => {
+  return (
+    <View style={styles.container}>
+      <Video
+        source={{ uri: url }}
+        style={styles.video}
+        resizeMode="contain"
+        controls={false}
+        paused={false}
+        repeat={false}
+      />
+    </View>
+  );
+};
+
+export default VideoNode;
+
+const styles = StyleSheet.create({
+  container: {
+    width,
+    height,
+    backgroundColor: '#000',
+  },
+
+  video: {
+    width: '100%',
+    height: '100%',
+  },
+});
